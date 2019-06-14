a33cd7ca706a89749ad2babb3a020d666d1f5a95
Rohit <rohit.r.baddam@gmail.com>
2019-06-14 10:35:31 -0400

    added framework for second controller method, will add more details when API is implemented

--- src/main/java/co/grandcircus/BaddamBoseTenbrick/MuralDisplayDetroit/MuralController.java
+++ src/main/java/co/grandcircus/BaddamBoseTenbrick/MuralDisplayDetroit/MuralController.java
@@ -1,16 +1,33 @@
 package co.grandcircus.BaddamBoseTenbrick.MuralDisplayDetroit;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.ModelAndView;
+
+import co.grandcircus.BaddamBoseTenbrick.MuralDisplayDetroit.entity.Mural;
+import co.grandcircus.BaddamBoseTenbrick.MuralDisplayDetroit.entity.MuralRepository;
 
 @Controller
 public class MuralController {
+	
+	@Autowired
+	MuralRepository mr;
 
 	@RequestMapping("/")
 	public ModelAndView homeTest() {
 
-		return new ModelAndView("index");
+		return new ModelAndView("Index");
 	}
 
+	@RequestMapping("/art_near_me")
+	public ModelAndView displayArt() {
+		List<Mural> murals = mr.findAll(); 
+		return new ModelAndView("artnearme", "murals", murals); 
+		
+	}
 }
