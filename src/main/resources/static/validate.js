bcdd05d4441164b43db7c8432a54255583c03a58
Rohit <rohit.r.baddam@gmail.com>
2019-06-19 16:10:00 -0400

    fixed the account creation validation

--- src/main/resources/static/validate.js
+++ src/main/resources/static/validate.js
@@ -1,9 +1,10 @@
 function validate() {
 	var pass = document.getElementsByName("password")[0].value;
-	var confirmpass = document.getElementByName("confirmpassword")[0].value; 
+	var confirmpass = document.getElementsByName("confirmpassword")[0].value; 
 	if (pass === confirmpass) {
 		return true; 
 	} else {
 		return false; 
 	}
 }
+
